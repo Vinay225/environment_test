0478a6da94425998e0d26fd23c9b0c7a97e94b92
Vinay225 <vinayadabala@gmail.com>
2021-09-30 18:33:16 -0500

    Final

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -2,11 +2,49 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a book', type: :feature do
-  scenario 'valid inputs' do
+  scenario 'valid Title' do
     visit new_book_path
-    fill_in 'Title', with: 'harry potter'
+    fill_in 'Title', with: 'Harry'
+    fill_in 'Author', with: 'JK Rowling'
+    fill_in 'Price', with: '20'
+    fill_in 'Published date', with: '2000-06-18'
     click_on 'Create Book'
     visit books_path
-    expect(page).to have_content('harry potter')
+    expect(page).to have_content('Harry')
   end
+
+
+  scenario 'valid Author' do
+    visit new_book_path
+    fill_in 'Title', with: 'Harry'
+    fill_in 'Author', with: 'JK Rowling'
+    fill_in 'Price', with: '20'
+    fill_in 'Published date', with: '2000-06-18'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('JK Rowling')
+  end
+
+  scenario 'valid price' do
+    visit new_book_path
+    fill_in 'Title', with: 'Harry'
+    fill_in 'Author', with: 'JK Rowling'
+    fill_in 'Price', with: '20'
+    fill_in 'Published date', with: '2000-06-18'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('20')
+  end
+
+  scenario 'valid date' do
+    visit new_book_path
+    fill_in 'Title', with: 'Harry'
+    fill_in 'Author', with: 'JK Rowling'
+    fill_in 'Price', with: '20'
+    fill_in 'Published date', with: '2000-06-18'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('2000-06-18')
+  end
+
 end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,15 +3,27 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(title: 'Percy Jackson', author: 'JK Rowling', published_date: '2000-06-18', price: '20')
   end
 
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
+  it 'is valid author with valid attributes' do
+    book1 = Book.new(title: 'Percy Jackson', author: 'JK Rowling', published_date: '2000-06-18', price: '20')
+    expect(book1).to be_valid
   end
 
-  it 'is not valid without a name' do
-    subject.title = nil
-    expect(subject).not_to be_valid
+  it 'is valid author with valid attributes' do
+    book2 = Book.new(title: 'Harry Potter', author: 'JK Rowling', published_date: '2000-06-18', price: '20')
+    expect(book2).to be_valid
   end
+
+  it 'is valid author with valid attributes' do
+    book3 = Book.new(title: 'Amazon Rainforest', author: 'JK Rowling', published_date: '2000-06-18', price: '20')
+    expect(book3).to be_valid
+  end
+
+  it 'is valid author with valid attributes' do
+    book4 = Book.new(title: 'BMW', author: 'JK Rowling', published_date: '2000-06-18', price: '20')
+    expect(book4).to be_valid
+  end
+
 end
