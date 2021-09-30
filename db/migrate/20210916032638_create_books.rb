24533d56c5b2f68b374a9a2e4d8336505d2e2350
Vinay225 <vinayadabala@gmail.com>
2021-09-30 10:22:43 -0500

    update

--- db/migrate/20210916032638_create_books.rb
+++ db/migrate/20210916032638_create_books.rb
@@ -2,8 +2,11 @@ class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
       t.string :title
-
+      t.string :author
+      t.numerical :price 
+      t.Datetime :published-date
       t.timestamps
     end
   end
 end
+   
